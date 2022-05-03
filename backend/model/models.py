a511d842533f18053cb3f4d9e4f5c7ceb18cc067
Crowdbotics <engineering@crowdbotics.com>
2022-05-03 12:24:54 +0000

    Update models

--- backend/model/models.py
+++ backend/model/models.py
@@ -3,5 +3,6 @@ from django.db import models
 class Newmod(models.Model):
     'Generated Model'
     name = models.BigIntegerField()
+    email = models.TextField(null=True,blank=True,)
 
 # Create your models here.
